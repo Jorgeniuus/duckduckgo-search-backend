28f459ce2e940190b34c417effdc64b2576e61c2
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-11-08 00:57:31 -0300

    feat: Added GET endpoint to consume external API

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const cors = require('cors')
 
+const axios = require('axios')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.get('/', (req, res) =>{
-    res.send({"name": "Jorge"})
+app.get('/search', async (req, res) =>{
+    const query = req.query.q
+    if(!query){
+        return res.status(400).json({error: 'The "q" parameter is required'})
+    }
+    try{
+        const api = `https://api.duckduckgo.com/?q=${query}&format=json`
+        const response = await axios.get(api, {
+            params:{
+                q: query,
+                format: 'json'
+            }
+        })
+        res.json(response.data.RelatedTopics.map(item => ({
+                title: item.Text,
+                url: item.FirstURL
+            }
+        )))
+    }catch (error){
+        res.status(500).json({error: 'something went wrong!'})
+    }
 })
 
 module.exports = app
