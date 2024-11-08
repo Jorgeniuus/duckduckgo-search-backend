3f22ba19d0da2d112684b1f78488baf5eed746a0
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-11-08 15:46:45 -0300

    feat: Improvement CORS security configuration with specific origin, methods and headers

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 app.use('/search', routes)
 
 module.exports = app
